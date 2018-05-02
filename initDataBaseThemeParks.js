eac8e95b2206e6733028f17c1f0eee6251897c31
FxBresson <fx-bresson@live.fr>
2018-05-02 18:15:25 +0200

    Add init file

---
+++ initDataBaseThemeParks.js
@@ -0,0 +1,43 @@
+// include the Themeparks library
+const Themeparks = require("themeparks");
+
+const dlpPark = new Themeparks.Parks.DisneylandParisMagicKingdom();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/dlp_test');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+
+    /*
+
+    Identifiant
+    -- Nom
+    -- Localisation
+    -- Sensation
+    -- Durée attraction
+    -- Taille autorisée
+    -- FastPass ?
+    -- Land
+
+    */
+  
+    let ridesSchema = mongoose.Schema({
+        _id: String,
+        name: String
+    })
+    let rideModel = mongoose.model('Ride', ridesSchema)
+    
+    dlpPark.GetWaitTimes().then(function(rides) {
+
+        for (ride of dlpPark.Rides) {
+            ride = ride.toJSON()
+            let rideObj = new rideModel({_id: ride.id})
+            rideObj.save(function (err, rideSaved) {
+                if (err) return console.error(err);
+            });
+        
+        }
+    }, console.error);
+});
