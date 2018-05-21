1e242f131c77c2733f65a17c9c168cbace3a9ba1
FxBresson <fx-bresson@live.fr>
2018-05-21 19:50:34 +0200

    Get walk time duration between two rides

--- data.js
+++ data.js
@@ -1,267 +1,267 @@
 var data = {
     "P2XA06": {
       "duration": 55,
-      "coordinates": [0, 0]
+      "coordinates": [48.866924, 2.776551]
     //   "name": "RC Racer"
     },
     "P1AA03": {
       "duration": 300,
-      "coordinates": [0, 0]
+      "coordinates": [48.872510, 2.775378]
     //   "name": "Le Passage Enchanté d'Aladdin"
     },
     "P1AA01": {
       "duration": 480,
-      "coordinates": [0, 0]
+      "coordinates": [48.872456, 2.773943]
     //   "name": "La Cabane des Robinson"
     },
     "P1AA04": {
       "duration": 540,
-      "coordinates": [0, 0] 
+      "coordinates": [48.873476, 2.773397] 
     //   "name": "Pirates of the Caribbean"
     },
     "P1AA02": {
       "duration": 74,
-      "coordinates": [0, 0] 
+      "coordinates": [48.872410, 2.772074] 
     //   "name": "Indiana Jones™ et le Temple du Péril"
     },
     "P1DA03": {
       "duration": 300,
-      "coordinates": [0, 0] 
+      "coordinates": [48.873413, 2.778792] 
     //   "name": "Autopia"
     },
     "P1DA06": {
       "duration": 300,
-      "coordinates": [0, 0] 
+      "coordinates": [48.873406, 2.779570] 
     //   "name": "Les Mystères du Nautilus"
     },
     "P1DA04": {
       "duration": 300,
-      "coordinates": [0, 0] 
+      "coordinates": [48.873413, 2.777996] 
     //   "name": "Buzz Lightyear Laser Blast"
     },
     "P1AA05": {
-      "duration": 0,
-      "coordinates": [0, 0] 
+      "duration": null,
+      "coordinates": [48.872784, 2.773637] 
     //   "name": "La Plage des Pirates"
     },
     "P1AA08": {
       "duration": null,
-      "coordinates": [0, 0] 
+      "coordinates": [48.872945, 2.773643] 
     //   "name": "Le Galion des Pirates"
     },
     "P1DA09": {
       "duration": 280,
-      "coordinates": [0, 0] 
+      "coordinates": [48.874622, 2.778782] 
     //   "name": "Star Tours : L’Aventure Continue"
     },
     "P1DA13": {
       "duration": 720,
-      "coordinates": [0, 0] 
+      "coordinates": [48.874442, 2.779747] 
     //   "name": "Star Wars: Path of the Jedi"
     },
     "P1DA10": {
       "duration": null,
-      "coordinates": [0, 0] 
+      "coordinates": [48.874670, 2.778408] 
     //   "name": "Disneyland Railroad Discoveryland® Station"
     },
     "P1DA07": {
       "duration": 90,
-      "coordinates": [0, 0] 
+      "coordinates": [48.873436, 2.778387] 
     //   "name": "Orbitron"
     },
     "P1DA08": {
       "duration": 180,
-      "coordinates": [0, 0] 
+      "coordinates": [48.873646, 2.778799] 
     //   "name": "Star Wars Hyperspace Mountain"
     },
     "P1MA04": {
       "duration": null,
-      "coordinates": [0, 0] 
+      "coordinates": [48.870967, 2.778920] 
     //   "name": "Main Street Vehicles"
     },
     "P1MA06": {
       "duration": null,
-      "coordinates": [0, 0] 
+      "coordinates": [48.874385, 2.773862] 
     //   "name": "Meet Mickey Mouse"
     },
     "P1MA05": {
       "duration": null,
-      "coordinates": [0, 0] 
+      "coordinates": [48.870921, 2.779077] 
     //   "name": "Disneyland Railroad Main Street Station"
     },
     "P1DA14": {
       "duration": null,
-      "coordinates": [0, 0] 
+      "coordinates": [48.874549, 2.779097] 
     //   "name": "Starport : rencontre avec un Personnage Star Wars"
     },
     "P1MA01": {
       "duration": null,
-      "coordinates": [0, 0] 
+      "coordinates": [48.870921, 2.779077] 
     //   "name": "Disneyland Railroad"
     },
     "P1NA02": {
       "duration": 120,
-      "coordinates": [0, 0] 
+      "coordinates": [48.873910, 2.775149] 
     //   "name": "Le Carrousel de Lancelot"
     },
     "P1NA03": {
       "duration": 130,
-      "coordinates": [0, 0] 
+      "coordinates": [48.875152, 2.774305] 
     //   "name": "Casey Jr. – le Petit Train du Cirque"
     },
     "P1NA09": {
       "duration": 300,
-      "coordinates": [0, 0] 
+      "coordinates": [48.875187, 2.774474] 
     //   "name": "Le Pays des Contes de Fées"
     },
     "P1NA05": {
       "duration": 90,
-      "coordinates": [0, 0] 
+      "coordinates": [48.874171, 2.774604] 
     //   "name": "Dumbo the Flying Elephant"
     },
     "P1NA00": {
       "duration": 600,
-      "coordinates": [0, 0] 
+      "coordinates": [48.874448, 2.774528] 
     //   "name": "Alice's Curious Labyrinth"
     },
     "P1NA01": {
       "duration": 160,
-      "coordinates": [0, 0] 
+      "coordinates": [48.873456, 2.775255] 
     //   "name": "Blanche-Neige et les Sept Nains"
     },
     "P1NA08": {
       "duration": 90,
-      "coordinates": [0, 0] 
+      "coordinates": [48.874342, 2.774947] 
     //   "name": "Mad Hatter's Tea Cups"
     },
     "P1NA17": {
       "duration": null,
-      "coordinates": [0, 0] 
+      "coordinates": [48.874744, 2.775571] 
     //   "name": "Pavillon des Princesses"
     },
     "P1NA10": {
       "duration": 180,
-      "coordinates": [0, 0] 
+      "coordinates": [48.873864, 2.774096] 
     //   "name": "Peter Pan's Flight"
     },
     "P1NA06": {
-      "duration": 600,
-      "coordinates": [0, 0] 
+      "duration": 300,
+      "coordinates": [48.873156, 2.776030] 
     //   "name": "La Galerie de la Belle au Bois Dormant"
     },
     "P1NA07": {
       "duration": 630,
-      "coordinates": [0, 0] 
+      "coordinates": [48.874692, 2.776144] 
     //   "name": "'it's a small world'"
     },
     "P1NA16": {
       "duration": null,
-      "coordinates": [0, 0] 
+      "coordinates": [48.874474, 2.773469] 
     //   "name": "Disneyland Railroad Fantasyland® Station"
     },
     "P1RA07": {
       "duration": null,
-      "coordinates": [0, 0] 
+      "coordinates": [48.870483, 2.774140] 
     //   "name": "Pocahontas Indian Village"
     },
     "P1RA00": {
       "duration": 240,
-      "coordinates": [0, 0] 
+      "coordinates": [48.871564, 2.774696] 
     //   "name": "Big Thunder Mountain"
     },
     "P1NA12": {
       "duration": null,
-      "coordinates": [0, 0] 
+      "coordinates": [48.873093, 2.775984] 
     //   "name": "La Tanière du Dragon"
     },
     "P1NA13": {
       "duration": 120,
-      "coordinates": [0, 0] 
+      "coordinates": [48.873565, 2.775160] 
     //   "name": "Les Voyages de Pinocchio"
     },
     "P1RA06": {
       "duration": 720,
-      "coordinates": [0, 0] 
+      "coordinates": [48.871184, 2.776460] 
     //   "name": "Thunder Mesa Riverboat Landing"
     },
     "P2XA03": {
       "duration": 140,
-      "coordinates": [0, 0] 
+      "coordinates": [48.868061, 2.778207] 
     //   "name": "Crush's Coaster"
     },
     "P1RA03": {
       "duration": 600,
-      "coordinates": [0, 0] 
+      "coordinates": [48.870972, 2.776820] 
     //   "name": "Phantom Manor"
     },
     "P1RA10": {
       "duration": null,
-      "coordinates": [0, 0] 
+      "coordinates": [48.870273, 2.772243] 
     //   "name": "Disneyland Railroad Frontierland® Depot"
     },
     "P1RA05": {
       "duration": null,
-      "coordinates": [0, 0] 
+      "coordinates": [48.871537, 2.775628] 
     //   "name": "Rustler Roundup Shootin' Gallery"
     },
     "P2XA02": {
       "duration": 90,
-      "coordinates": [0, 0] 
+      "coordinates": [48.867669, 2.777883] 
     //   "name": "Cars Quatre Roues Rallye"
     },
     "P2XA09": {
       "duration": 0,
-      "coordinates": [0, 0] 
+      "coordinates": [48.867550, 2.776249] 
     //   "name": "Ratatouille : L'Aventure Totalement Toquée de Rémy"
     },
     "P2XA00": {
       "duration": 280,
-      "coordinates": [0, 0] 
+      "coordinates": [48.866776, 2.778131] 
     //   "name": "Studio Tram Tour : Behind the Magic"
     },
     "P2XA01": {
       "duration": 1500,
-      "coordinates": [0, 0] 
+      "coordinates": [48.867288, 2.778687] 
     //   "name": "Art of Disney Animation"
     },
     "P2XA05": {
       "duration": 90,
-      "coordinates": [0, 0] 
+      "coordinates": [48.868182, 2.778545] 
     //   "name": "Les Tapis Volants – Flying Carpets Over Agrabah"
     },
     "P2XA08": {
       "duration": 90,
-      "coordinates": [0, 0] 
+      "coordinates": [48.867125, 2.776977] 
     //   "name": "Slinky Dog Zigzag Spin"
     },
     "P2XA07": {
       "duration": 90,
-      "coordinates": [0, 0] 
+      "coordinates": [48.867206, 2.777287] 
     //   "name": "Toy Soldiers Parachute Drop"
     },
     "P2YA01": {
       "duration": null,
-      "coordinates": [0, 0] 
+      "coordinates": [48.865384, 2.780006] 
     //   "name": "Meet Spider-Man"
     },
     "P1AA00": {
       "duration": null,
-      "coordinates": [0, 0] 
+      "coordinates": [48.872565, 2.773636] 
     //   "name": "Adventure Isle"
     },
     "P2ZA00": {
       "duration": 1080,
-      "coordinates": [0, 0] 
+      "coordinates": [48.865888, 2.779343] 
     //   "name": "Armageddon : les Effets Spéciaux"
     },
     "P2ZA01": {
       "duration": 120,
-      "coordinates": [0, 0] 
+      "coordinates": [48.865281, 2.779457] 
     //   "name": "Rock 'n' Roller Coaster avec Aerosmith"            
     },
     "P2ZA02": {
       "duration": 300,
-      "coordinates": [0, 0] 
+      "coordinates": [48.866746, 2.778772] 
     //   "name": "The Twilight Zone Tower of Terror™"
     }
   }
